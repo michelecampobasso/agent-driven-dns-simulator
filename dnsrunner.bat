4f025c64c458e665242b074174b5e01abf1e518d
DESKTOP-CUSAUGR <michele.campobasso3@gmail.com>
2017-03-07 09:05:07 +0100

    dnsrunner.sh created, dnsrunner.bat fixed, shortened tldhosts.txt (a copy will be present on VM with different addresses), ClientAgent now demands to TheInternet to have hosts at post-startup

--- dnsrunner.bat
+++ dnsrunner.bat
@@ -8,11 +8,4 @@ RootServer:dns.server.agents.RootServerAgent;^
 133.133.133.134:dns.server.agents.DNSServerAgent();^
 144.144.144.143:dns.server.agents.DNSServerAgent();^
 144.144.144.144:dns.server.agents.DNSServerAgent();^
-210.210.210.210:dns.client.agents.ClientAgent();^
-222.222.222.222:dns.server.agents.TopLevelDomainServerAgent();^
-223.223.223.223:dns.server.agents.TopLevelDomainServerAgent();^
-231.231.231.231:dns.server.agents.DNSServerAgent();^
-231.231.231.232:dns.server.agents.DNSServerAgent();^
-244.244.244.243:dns.server.agents.DNSServerAgent();^
-244.244.244.244:dns.server.agents.DNSServerAgent();^
 GOD:dns.theinternet.TheInternetAgent -local-host 192.168.56.1 -local-port 1099 -gui &
--- src/dns/client/agents/ClientAgent.java
+++ src/dns/client/agents/ClientAgent.java
@@ -7,32 +7,80 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import dns.client.behaviours.*;
+import dns.tables.Host;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 public class ClientAgent extends Agent {
 
 	private static final long serialVersionUID = 271669436955865796L;
 	private ArrayList<String> hosts = new ArrayList<String>();
+
+	private MessageTemplate mtInternet = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM), 
+			MessageTemplate.MatchOntology("ALLHOSTSPLEASE"));
 	
-    @Override
+	private DFAgentDescription template;
+	private ServiceDescription sd;
+	private SearchConstraints all;
+	
+	
+    @SuppressWarnings("unchecked")
+	@Override
     protected void setup() {
-        
-        // Carico gli hosts...
+    	
+    	if (getArguments()[0].toString().equalsIgnoreCase("post")) {
+    		
+    		/*
+    		 * Vado alla ricerca dell'agente TheInternet per ottenere la cache di tutti gli hosts conosciuti.
+    		 */
+    		
+    		template = new DFAgentDescription();
+		    sd = new ServiceDescription();
+		    sd.setType("THEINTERNET");
+		    template.addServices(sd);
+		    all = new SearchConstraints();
+		    all.setMaxResults(new Long(-1));
+		    
+		    DFAgentDescription[] result = null;
+		    ArrayList<Host> cachedHostTable = null;
+		    try {
+		        result = DFService.search(this, template, all);
+		        if (result.length!=0) {
+		        	ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+				    request.setOntology("ALLHOSTSPLEASE");
+				    request.addReceiver(result[0].getName());
+				    send(request);
+				    
+				    ACLMessage response = blockingReceive(mtInternet, 5000);
+				    cachedHostTable = (ArrayList<Host>) response.getContentObject();
+		        }
+		    } catch (final FIPAException fe) {
+		        fe.printStackTrace();
+		    } catch (UnreadableException e) {
+				e.printStackTrace();
+			}
+		    
+		    /*
+		     * Carico nell'agente tutti gli hostnames creati dall'agente TheInternet.
+		     */
+		    if (cachedHostTable != null)
+		    	for (Host h : cachedHostTable)
+		    		hosts.add(h.getName());
+		
+    	}
+    	// Carico gli hosts...
  		try { 
  			BufferedReader br = new BufferedReader(new FileReader("hosts.txt"));
  	        String line = null;
  	        while ((line = br.readLine()) != null)
  	        	hosts.add(line);
- 	        br.close();
- 	        // ...anche quelli aggiunti se questo agente sta venendo avviato in un secondo momento
- 	        br = new BufferedReader(new FileReader("added_hosts.txt"));
- 	        line = null;
- 	        while ((line = br.readLine()) != null)
- 	        	hosts.add(line.split("\\s+")[0]);
  	        br.close();
  	        
  		}
@@ -42,7 +90,6 @@ public class ClientAgent extends Agent {
  		catch (IOException e) {
  			e.printStackTrace();
  		}
- 		
  		/*
 		 * Registrazione al DF...
 		 */
